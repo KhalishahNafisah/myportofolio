d5b09a5809030e799aa9a82ff1f6855e76c7b2f2
Khalishah Nafisah <khalishah.nafisah@ui.ac.id>
2026-09-09 17:15:04 +0700

    Tutorial 02

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Khalishah",
+        "npm": "2506605840",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa Sistem Informasi Universitas Indonesia yang tertarik "
+            "pada Project and Product Management."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Khalishah",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
+
